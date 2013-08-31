c527f0057a4ae27e17b7ce59cf7ea588fc149feb
Francisco Dias <xicombd@gmail.com>
2013-08-31 06:11:55 +0100

    Added engine controller

--- controllers/engine.js
+++ controllers/engine.js
@@ -1,38 +1,43 @@
-module.exports = function () {
+var five = require('johnny-five')
+  , board = require('./../controlCenter')
 
-  var version = "0.0.0"
-    , id = "engine"
+var version = "0.0.0"
+  , id = "engine"
 
-  var five, board, servo, pin = 10
+var pin = 10
+var servo = new five.Servo(pin)
+var speed = 30
 
-  var init = function (_five, _board) {
-    five = _five
-    board = _board    
-    servo = new five.Servo(pin)
-  }
-  
-  // this.inject = function () {
-  // board.repl.inject({
-  //      b: this.servo
-  //    })
-  //  }
+// this.inject = function () {
+// board.repl.inject({
+//      b: this.servo
+//    })
+//  }
 
-  var setSpeed = function (degree){
-    servo.move(degree)
-  }
-
-  var stop = function() {
-    servo.center()
-  }
-
-  var forward = function () {
-    servo.move(135)  
-  }
-
-  var backward = function () {
-    servo.move(45)  
-  }
+var setSpeed = function (value){
+	// from 0 to 45
+	if(value > 45){ value = 45 }
+	if(value < 0){ value = 0 }
+	
+    speed = value
 }
 
+var stop = function() {
+	servo.center()
+}
 
+var forward = function () {
+	servo.move(90+speed)  
+}
 
+var backward = function () {
+	servo.move(90-speed)  
+}
+
+module.exports = {
+  setSpeed: setSpeed,
+  stop: stop,
+  forward: forward,
+  backward: backward,
+  id: id
+}
