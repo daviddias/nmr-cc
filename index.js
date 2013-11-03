b5cec49d734b9d79c7c44f6881d34085bbeb2c67
David Dias <daviddias.p@gmail.com>
2013-11-03 13:19:32 +0000

    added a callback

--- index.js
+++ index.js
@@ -3,34 +3,29 @@ var io = require('socket.io-client')
   , fs = require('fs');
 
 
-module.exports = function (carIP, carPort) {
-	var socket = io.connect(carIP, {
-	  port: carPort
-	});
+module.exports = function (kartIP, carPort, cb) {
+  var socket = io.connect(kartIP, {
+    port: carPort
+  });
 
-	// var socket = io.connect('192.168.0.102', {
-	//   port: 8888
-	// });
+  socket.on('connect', function () {
+    console.log('socket connected');
 
-	socket.on('connect', function () {
-	  console.log('socket connected');
+    var sendController = function (filepath, filename){
+      var stream = ss.createStream();
+      ss(socket).emit('addController', stream, {name: filename});
+      fs.createReadStream(filepath).pipe(stream);
+    };
 
-		var sendController = function (filepath, filename){
-			var stream = ss.createStream();
-			ss(socket).emit('addController', stream, {name: filename});
-			fs.createReadStream(filepath).pipe(stream);
-		};
+    var sendCommand = function (options) {
+      socket.emit('execute', options);
+    };
+    
+    cb({ sendController: sendController,
+         sendCommand: sendCommand
+    });
 
-		var sendCommand = function (options) {
-			socket.emit('execute', options);
-		};
-
-    return {
-			sendController: sendController,
-			sendCommand: sendCommand
-		};
-
-	});
+  });
 };
 
 
